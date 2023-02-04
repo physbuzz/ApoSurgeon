50424c21e2f4f07f9b00d458dbe251b1e7a9e402
BT100 Building <BT100@ac.sdmesa.net>
2023-02-04 14:28:29 -0800

    hudcontroller

---
+++ Assets/UI/HUDController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+//using UnityEngine.UIElements;
+
+public class HUDController : MonoBehaviour
+{
+    public ProgressBar Blood;
+    public Button test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        Blood = root.Q<ProgressBar>("BloodBar");
+        Blood.value = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Blood.value += 1;
+    }
+}
